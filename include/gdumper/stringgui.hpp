b1ad980b709214d4c793673a78c6f62aa1d12ec1
ItalianG0urmet <insogna_@outlook.it>
2025-10-01 22:59:02 +0200

    Make basic functions to string-gui

--- include/gdumper/stringgui.hpp
+++ include/gdumper/stringgui.hpp
@@ -1,26 +1,34 @@
 #pragma once
 
+#include <expected>
+#include <string>
+#include <vector>
+
 #include "gui.hpp"
 
 class StringGui : public Gui {
    public:
-    StringGui(pid_t pid)
-        : currentLine_(0),
-          maxLine_(0),
-          pid_(pid) {}
+    StringGui(pid_t pid) : currentLine_(0), maxLine_(0), pid_(pid) {}
 
    private:
+    // Override null
+    void onResize() override {}
+
     // Override
-    void onResize() override;
     std::expected<ReturnSignal, std::string> handleKeys(int ch) override;
-    std::expected<void, std::string> onRun() override;
     std::expected<void, std::string> beforeRefresh() override;
+    std::expected<void, std::string> onRun() override;
+
+    // Info function
+    std::expected<void, std::string> loadProgramStrings();
+    void loadLines() const;
 
     // Render var
     int currentLine_ = 0;
     int maxLine_ = 0;
 
     // Settings
+    std::vector<std::string> programStrings_;
+    std::string exePath_{};
     pid_t pid_ = 0;
-
 };
--- src/gui/stringgui.cpp
+++ src/gui/stringgui.cpp
@@ -1,8 +1,16 @@
 #include "gdumper/stringgui.hpp"
 
+#include <expected>
+#include <string>
+
 #include "gdumper/gui.hpp"
 
-void StringGui::onResize() {}
+std::expected<void, std::string> StringGui::loadProgramStrings() {
+ 
+    // Todo: System that laad all the strings in the vector
+
+    return {};
+}
 
 std::expected<Gui::ReturnSignal, std::string> StringGui::handleKeys(int ch) {
     switch (ch) {
@@ -12,17 +20,59 @@ std::expected<Gui::ReturnSignal, std::string> StringGui::handleKeys(int ch) {
         case '\t':
             return NEXT_SIG;
             break;
+        case KEY_UP:
+        case 'k':
+            if (currentLine_ > 0) currentLine_--;
+            break;
+        case KEY_DOWN:
+        case 'j':
+            currentLine_ = std::min(
+                currentLine_ + 1,
+                static_cast<int>(programStrings_.size()) - maxLine_ - 1);
+            break;
     }
     return {};
 }
 
 std::expected<void, std::string> StringGui::onRun() {
     footer_ = "[Arrows] Navigate | [q] Quit";
+    header_ = " PID: " + std::to_string(pid_);
+    exePath_ = "/proc/" + std::to_string(pid_) + "/exe";
 
-    currentLine_ = 0;
+    if (const auto check = loadProgramStrings(); !check) {
+        return std::unexpected(check.error());
+    }
 
     return {};
 }
 
-std::expected<void, std::string> StringGui::beforeRefresh() { return {}; }
+void StringGui::loadLines() const {
+    const int maxVisibleLines{LINES - 6};
+    const int start{currentLine_};
+    const int end{std::min(start + maxVisibleLines,
+                           static_cast<int>(programStrings_.size()))};
 
+    for (int i = 0; i < end - start; i++) {
+        int lineY = 3 + i;
+        int globalIndex = start + i;
+
+        attron(COLOR_PAIR(LINE_NUMBER));
+        mvprintw(lineY, 2, "%04d", globalIndex + 1);
+
+        attron(COLOR_PAIR(DEFAULT_TEXT));
+        std::string text = programStrings_[globalIndex];
+        text.resize(COLS - 10, ' ');
+        mvprintw(lineY, 10, "%s", text.c_str());
+
+        if (globalIndex == currentLine_) {
+            attron(COLOR_PAIR(HIGHLIGHT));
+            mvprintw(lineY, 8, ">");
+            attroff(COLOR_PAIR(HIGHLIGHT));
+        }
+    }
+}
+
+std::expected<void, std::string> StringGui::beforeRefresh() {
+    loadLines();
+    return {};
+}
